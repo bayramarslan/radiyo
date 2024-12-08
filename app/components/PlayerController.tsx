8199bbe1d3e9a4b681b951f288cb766ac8838c4c
Bayram Arslan <bayramweb@gmail.com>
2024-12-08 04:19:28 +0300

    wip

--- app/components/PlayerController.tsx
+++ app/components/PlayerController.tsx
@@ -1,33 +1,50 @@
+import Image from "next/image";
 import React from "react";
 
 type PlayerControllerProps = {
   isPlay: boolean;
   handlePlay: () => void;
   handlePause: () => void;
+  station: RadioStation;
 };
 
 const PlayerController: React.FC<PlayerControllerProps> = ({
   isPlay,
   handlePlay,
   handlePause,
+  station,
 }) => {
   return (
-    <div className="flex flex-row gap-2 items-center justify-center">
-      <div className="flex gap-4 items-center justify-center">
+    <div className="flex flex-1 flex-row items-center justify-between gap-2 px-4 py-3">
+      <div className="flex items-center justify-center">
+        <Image
+          src={station.icon}
+          width={60}
+          height={60}
+          alt={`${station.name} Logo`}
+          className="mr-3 rounded-lg border border-white/20"
+        />
+        <div className="">
+          <p className="font-bold">{station.name}</p>
+          <p className="text-sm opacity-70">{station.homepage}</p>
+        </div>
+      </div>
+      <div className="flex items-center justify-center gap-4 text-orange-600 dark:text-zinc-500">
         {isPlay && (
           <button
             onClick={handlePause}
-            className="dark:bg-gray-700 bg-gray-300 px-8 py-3 rounded-full"
+            className="flex size-12 items-center justify-center rounded-lg transition-all hover:bg-orange-600 hover:text-white"
           >
             <svg
               xmlns="http://www.w3.org/2000/svg"
-              width="20"
-              height="20"
-              fill="currentColor"
-              viewBox="0 0 256 256"
-              data-darkreader-inline-fill=""
+              width="16"
+              height="16"
+              viewBox="0 0 24 24"
             >
-              <path d="M200,32H160a16,16,0,0,0-16,16V208a16,16,0,0,0,16,16h40a16,16,0,0,0,16-16V48A16,16,0,0,0,200,32Zm0,176H160V48h40ZM96,32H56A16,16,0,0,0,40,48V208a16,16,0,0,0,16,16H96a16,16,0,0,0,16-16V48A16,16,0,0,0,96,32Zm0,176H56V48H96Z"></path>
+              <path
+                fill="currentColor"
+                d="M2 6c0-1.886 0-2.828.586-3.414S4.114 2 6 2s2.828 0 3.414.586S10 4.114 10 6v12c0 1.886 0 2.828-.586 3.414S7.886 22 6 22s-2.828 0-3.414-.586S2 19.886 2 18zm12 0c0-1.886 0-2.828.586-3.414S16.114 2 18 2s2.828 0 3.414.586S22 4.114 22 6v12c0 1.886 0 2.828-.586 3.414S19.886 22 18 22s-2.828 0-3.414-.586S14 19.886 14 18z"
+              />
             </svg>
           </button>
         )}
@@ -35,17 +52,18 @@ const PlayerController: React.FC<PlayerControllerProps> = ({
         {!isPlay && (
           <button
             onClick={handlePlay}
-            className="dark:bg-gray-700 bg-gray-300 px-8 py-3 rounded-full"
+            className="flex size-12 items-center justify-center rounded-lg transition-all hover:bg-orange-600 hover:text-white"
           >
             <svg
               xmlns="http://www.w3.org/2000/svg"
-              width="20"
-              height="20"
-              fill="currentColor"
-              viewBox="0 0 256 256"
-              data-darkreader-inline-fill=""
+              width="16"
+              height="16"
+              viewBox="0 0 24 24"
             >
-              <path d="M232.4,114.49,88.32,26.35a16,16,0,0,0-16.2-.3A15.86,15.86,0,0,0,64,39.87V216.13A15.94,15.94,0,0,0,80,232a16.07,16.07,0,0,0,8.36-2.35L232.4,141.51a15.81,15.81,0,0,0,0-27ZM80,215.94V40l143.83,88Z"></path>
+              <path
+                fill="currentColor"
+                d="M21.409 9.353a2.998 2.998 0 0 1 0 5.294L8.597 21.614C6.534 22.737 4 21.277 4 18.968V5.033c0-2.31 2.534-3.769 4.597-2.648z"
+              />
             </svg>
           </button>
         )}
---
+++ app/types/index.d.ts
@@ -0,0 +1,8 @@
+interface RadioStation {
+  name: string;
+  url: string;
+  homepage: string;
+  icon: string;
+  country: string;
+  country_code: string;
+}
