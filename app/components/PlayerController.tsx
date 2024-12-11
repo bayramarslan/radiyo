2d9751ab4a49086ff7ee00bb31ba664c2994c692
Bayram Arslan <bayramweb@gmail.com>
2024-12-11 17:57:15 +0300

    add tabler icons

--- app/components/PlayerController.tsx
+++ app/components/PlayerController.tsx
@@ -1,3 +1,7 @@
+import {
+  IconPlayerPauseFilled,
+  IconPlayerPlayFilled,
+} from "@tabler/icons-react";
 import Image from "next/image";
 import React from "react";
 
@@ -35,37 +39,17 @@ const PlayerController: React.FC<PlayerControllerProps> = ({
         {isPlay && (
           <button
             onClick={handlePause}
-            className="flex size-12 items-center justify-center rounded-lg transition-all hover:bg-secondary/50"
+            className="flex size-12 items-center justify-center rounded-lg p-3.5 transition-all hover:bg-secondary/50"
           >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="18"
-              height="18"
-              viewBox="0 0 24 24"
-            >
-              <path
-                fill="currentColor"
-                d="M2 6c0-1.886 0-2.828.586-3.414S4.114 2 6 2s2.828 0 3.414.586S10 4.114 10 6v12c0 1.886 0 2.828-.586 3.414S7.886 22 6 22s-2.828 0-3.414-.586S2 19.886 2 18zm12 0c0-1.886 0-2.828.586-3.414S16.114 2 18 2s2.828 0 3.414.586S22 4.114 22 6v12c0 1.886 0 2.828-.586 3.414S19.886 22 18 22s-2.828 0-3.414-.586S14 19.886 14 18z"
-              />
-            </svg>
+            <IconPlayerPauseFilled className="text-foreground/80" />
           </button>
         )}
         {!isPlay && (
           <button
             onClick={handlePlay}
-            className="flex size-12 items-center justify-center rounded-lg transition-all hover:bg-secondary/50"
+            className="flex size-12 items-center justify-center rounded-lg p-3.5 transition-all hover:bg-secondary/50"
           >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="18"
-              height="18"
-              viewBox="0 0 24 24"
-            >
-              <path
-                fill="currentColor"
-                d="M21.409 9.353a2.998 2.998 0 0 1 0 5.294L8.597 21.614C6.534 22.737 4 21.277 4 18.968V5.033c0-2.31 2.534-3.769 4.597-2.648z"
-              />
-            </svg>
+            <IconPlayerPlayFilled className="text-foreground/80" />
           </button>
         )}
       </div>
