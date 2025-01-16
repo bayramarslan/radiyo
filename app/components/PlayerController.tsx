72ac354ab6968243d1a2e6058cc03727b5141d59
Bayram Arslan <bayramweb@gmail.com>
2025-01-17 01:00:57 +0300

    wip

--- app/components/PlayerController.tsx
+++ app/components/PlayerController.tsx
@@ -1,9 +1,16 @@
-import { PauseIcon, PlayIcon } from "@heroicons/react/16/solid";
+import {
+	BackwardIcon,
+	ForwardIcon,
+	PauseIcon,
+	PlayIcon,
+} from "@heroicons/react/16/solid";
 import Image from "next/image";
 
 type PlayerControllerProps = {
 	isPlay: boolean;
 	playStream: (station: RadioStation) => void;
+	nextStation: RadioStation | undefined;
+	prevStation: RadioStation | undefined;
 	station: RadioStation;
 };
 
@@ -11,6 +18,8 @@ export default function PlayerController({
 	isPlay,
 	playStream,
 	station,
+	prevStation,
+	nextStation,
 }: PlayerControllerProps) {
 	return (
 		<div className="sticky bottom-0 flex flex-row items-center justify-between gap-2 border-t-4 border-secondary bg-primary/30 px-4 py-3 backdrop-blur-3xl">
@@ -32,16 +41,28 @@ export default function PlayerController({
 					</p>
 				</div>
 			</div>
-			<div className="flex items-center justify-center gap-4 text-foreground/70">
+			<div className="flex items-center justify-center gap-1 text-foreground/70">
+				<button
+					onClick={() => prevStation && playStream(prevStation)}
+					className="flex size-10 items-center justify-center rounded outline-none transition-all hover:bg-secondary/15 focus:bg-secondary/15"
+				>
+					<BackwardIcon width={16} height={16} />
+				</button>
 				<button
 					onClick={() => playStream(station)}
-					className="flex size-12 items-center justify-center rounded-lg outline-none transition-all hover:bg-secondary/30 focus:bg-secondary/30"
+					className="flex size-12 items-center justify-center rounded outline-none transition-all hover:bg-secondary/15 focus:bg-secondary/15"
 				>
 					{isPlay ? (
 						<PauseIcon className="text-foreground/80" width={20} height={20} />
 					) : (
 						<PlayIcon className="text-foreground/80" width={20} height={20} />
 					)}
+				</button>
+				<button
+					onClick={() => nextStation && playStream(nextStation)}
+					className="flex size-10 items-center justify-center rounded outline-none transition-all hover:bg-secondary/15 focus:bg-secondary/15"
+				>
+					<ForwardIcon width={16} height={16} />
 				</button>
 			</div>
 		</div>
--- app/pages/Main.tsx
+++ app/pages/Main.tsx
@@ -4,7 +4,7 @@ import PlayerController from "@/components/PlayerController";
 import PlayerList from "@/components/PlayerList";
 import radioStations from "@/data/radioStations.json";
 import usePlayer from "@/hooks/usePlayer";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 const getSortedStations = (): RadioStation[] =>
 	radioStations
@@ -13,9 +13,23 @@ const getSortedStations = (): RadioStation[] =>
 
 export default function Main() {
 	const { isPlay, currentStation, playStream } = usePlayer();
+	const allStations = getSortedStations();
+	const [nextStation, setNextStation] = useState<RadioStation | undefined>();
+	const [prevStation, setPrevStation] = useState<RadioStation | undefined>();
 
 	const [filteredStations, setFilteredStations] =
-		useState<RadioStation[]>(getSortedStations());
+		useState<RadioStation[]>(allStations);
+
+	useEffect(() => {
+		if (!currentStation) return;
+
+		const currentIndex = allStations.indexOf(currentStation);
+		const nextStation = allStations[currentIndex + 1] || undefined;
+		const prevStation = allStations[currentIndex - 1] || undefined;
+
+		setNextStation(nextStation);
+		setPrevStation(prevStation);
+	}, [allStations, currentStation]);
 
 	const handleSearchChange = (searchTerm: string = "") => {
 		if (searchTerm.trim().length === 0) {
@@ -47,6 +61,8 @@ export default function Main() {
 					isPlay={isPlay}
 					playStream={playStream}
 					station={currentStation}
+					nextStation={nextStation}
+					prevStation={prevStation}
 				/>
 			)}
 		</div>
