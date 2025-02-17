da1b5d3ec057e6afaab9688de7e85ba9918dc4f7
Bayram Arslan <bayramweb@gmail.com>
2025-02-17 10:52:22 +0300

    wip

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,6 @@
 "use client";
 import Main from "@/pages/Main";
-import { Suspense } from "react";
 
 export default function Home() {
-	return (
-		<Suspense fallback={<p>Loading...</p>}>
-			<Main />
-		</Suspense>
-	);
+	return <Main />;
 }
